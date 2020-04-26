fda0c79de50692a020e2e18b70779c0dd84d88aa
Schulberg <jschulberg@deloitte.com>
2020-04-25 21:51:23 -0400

    First pass at data cleaning code

--- DOJ Antitrust Data Cleaning.R
+++ DOJ Antitrust Data Cleaning.R
@@ -21,18 +21,10 @@ suppressMessages(library("lubridate")) # Used to fix dates
 suppressMessages(library("tidytext")) # Used for text analysis/NLP
 suppressMessages(library("textdata")) # Used for sentiment analysis
 
-# Remove anything in our global environment
-rm(list = ls())
 
-# Set our folder
-wd <- "C:/Users/jschulberg/Documents/Data Analytics Blog/Blog 1 - DOS Travel Info/"
-setwd(wd)
-
-# Read our data in using the fromJSON function from the rjson package
-data <- fromJSON(file = "countrytravelinfo.json")
 
 # Convert to a tibble
-data <- as_tibble(data)
+data <- as_tibble(fy18ws)
 head(data)
 
 # As we can see from the print out above, our dataset is a list of
